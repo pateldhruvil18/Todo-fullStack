3ad6e3837552b0db8573af7e3fb541372606aa53
pateldhruvil18 <DHP204600@GMAIL.COM>
2026-03-17 12:30:21 +0530

    deleted unwanted files

--- Backend/api/routes/index.js
+++ Backend/api/routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 const authRoutes = require("./auth.routes");
 const todoRoutes = require("./todo.routes");
-const uploadRoutes = require("./upload.routes");
 
 router.use("/auth", authRoutes);
 router.use("/todos", todoRoutes);
-router.use("/upload", uploadRoutes);
 
 module.exports = router;
